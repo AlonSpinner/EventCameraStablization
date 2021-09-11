16f05739ac042496a7af01d4eedafa033dfa4f17
Alon <alonspinner@gmail.com>
2021-09-11 11:19:03 +0300

    RL fails - 3D simulation/memory

--- General Functions and Scripts/RLsetup.m
+++ General Functions and Scripts/RLsetup.m
@@ -9,13 +9,13 @@ actionInfo = rlNumericSpec([3 1],...
 actionInfo.Name = 'observations';
 actionInfo.Description = 'integrated error, error, and measured height';
 
-observationInfo = rlNumericSpec([144 256 6],...
+observationInfo = rlNumericSpec([144 256 1],...
     'LowerLimit',0,...
     'UpperLimit',1);
 observationInfo.Name = 'EventMask';
 observationInfo.Description = 'values of 0,0.5,1 where 0.5 indicates low or no rate of change';
 
-initOpts = rlAgentInitializationOptions('NumHiddenUnit',128);
+initOpts = rlAgentInitializationOptions('NumHiddenUnit',64);
 rng(0); %repeatable randomness
 agent = rlDDPGAgent(observationInfo,actionInfo,initOpts); %not rlDDPGAgent
 agent.AgentOptions.SampleTime=getDictionaryDesignData('ts_camera');
@@ -23,14 +23,17 @@ agent.AgentOptions.SampleTime=getDictionaryDesignData('ts_camera');
 actor=getActor(agent);
 actorNet = getModel(actor);
 actor.Options.LearnRate=1e-3;
-actor.Options.UseDevice="gpu";
+actor.Options.UseDevice="cpu";
 agent = setActor(agent,actor);
 
 critic=getCritic(agent);
 criticNet = getModel(critic);
 critic.Options.LearnRate=1e-3;
-critic.Options.UseDevice="gpu";
+critic.Options.UseDevice="cpu";
 agent = setCritic(agent,critic);
+
+agent.AgentOptions.MiniBatchSize=4;
+agent.AgentOptions.ExperienceBufferLength=100;
 
 %assign agent to dictionary
 mWS = get_param('Simulation','modelworkspace');
@@ -38,14 +41,14 @@ mWS.assignin('agent',agent);
 save_system(mdl);
 disp("Agent built and uploaded to model. Model was saved");
 %%  Train Agent
-rlAgentBlocks=[mdl,'/Estimator/RL/RL Agent'];
+rlAgentBlocks=[mdl,'/Estimator/RL - all/RL Agent'];
 env = rlSimulinkEnv(mdl,rlAgentBlocks,observationInfo,actionInfo);
 maxrIC=getDictionaryDesignData('maxAngles');
 env.ResetFcn = @(in)setVariable(in,'A0',0.7*maxrIC.*rand([1,3]).*randSign([1,3]),'Workspace',mdl);
 
 trainOpts = rlTrainingOptions(...
     'MaxEpisodes',10000,...
-    'MaxStepsPerEpisode',40,...
+    'MaxStepsPerEpisode',20,...
     'ScoreAveragingWindowLength',5,...
     'Plots','training-progress');
 
